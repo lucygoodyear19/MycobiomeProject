5c7312fad789181d0d6d79a84afed767ca1426e1
Lucy Goodyear <lucy.goodyear19@imperial.ac.uk>
2020-08-26 20:41:19 +0100

    Remove redundant code

--- Analysis/Generalised_Scripts/Analysis/cooccur.R
+++ Analysis/Generalised_Scripts/Analysis/cooccur.R
@@ -23,27 +23,36 @@ args = commandArgs(trailingOnly=TRUE) # setup to accept arguments from command l
 # test if there is at least one argument: if not, return an error
 if (length(args)==0) {
   stop("At least one argument must be supplied in the form of an R-script containing the following arguments: 
-       1) path containing results from DADA2 pipeline")
+       1) dada2_data_path - path to directory containing input data (phyloseq object)")
 }
 # load arguments into script
 source(args)
 # print arguments as check
-print(path_out)
+print(dada2_data_path)
+print(results_path)
 
 # load phyloseq object for DADA2 pipeline
-dada2 <- readRDS(paste0(path_out, "physeqob_DADA2.rds"))
+dada2 <- readRDS(paste0(dada2_data_path, "physeqob_DADA2.rds"))
+
 print("Data loaded")
 
 
 ##############################################################################
-########################### Calculate cooccurence ############################
+################################ Co-occurence ################################
 
 
 # extract Bd column from sample data
-bd <- sample_data(dada2)$Bd
+samps <- as(sample_data(dada2), "data.frame")
+for (sam in 1:nrow(samps)) {
+  if (!is.na(samps$Bd[sam]) & samps$Bd[sam] == "S") {samps$Bd[sam] <- 0}
+}
+bd <- samps$Bd
 
 # add Bd binary column to otu_table
 cooccur_object <- cbind(as.data.frame(otu_table(dada2)), bd)
+
+# remove samples with NA Bd qPCR
+cooccur_object <- cooccur_object[!is.na(cooccur_object$bd),]
 
 # set Bd column to numeric
 cooccur_object[,ncol(cooccur_object)] <- sapply(cooccur_object[,ncol(cooccur_object)], as.character)
@@ -58,7 +67,7 @@ cooccur_object_t$sum <- rowSums(cooccur_object_t)
 cooccur_object_t <- subset(cooccur_object_t, sum >= 2)
 cooccur_object_t <- subset(cooccur_object_t, select=-c(sum))
 
-# main coocur function to calculate pairwise cooccurence patterns
+# main cooccur function to calculate pairwise co-occurrence patterns
 print("Cooccur function start:")
 Sys.time()
 cooccur_full <- cooccur(cooccur_object_t, 
@@ -68,34 +77,8 @@ cooccur_full <- cooccur(cooccur_object_t,
 print("Cooccur function finished:")
 Sys.time()
 
-
-cooccur_full <- readRDS("/Users/lucy/Documents/MRes/MycobiomeProject/Analysis/Results/Taiwan_2016/Cooccur/cooccur_full_dada2.rds")
-
-
-##############################################################################
-################################# Bd Analysis ################################
-
-
-# look at cooccurence patterns just for Bd
-cooccur_bd <- pair(cooccur_full, spp="bd")
-
-# view all significant results for bd
-print(cooccur_bd)
-summary(cooccur_bd)
-pair.profile()
-
-
-sort(cooccur_bd, )
-
-# plot as a heatmap
-heatmap_bd <- plot(cooccur_bd)
-
-
-##############################################################################
-################################## Save outputs ##############################
-
-
-saveRDS(cooccur_full, paste0(path_out,"cooccur_full_dada2.rds"))
+# save cooccur object
+saveRDS(cooccur_full, paste0(results_path,"cooccur_full_dada2.rds"))
 
 
 ## end of script
