d1fa5124009209e27233d625c33a1ec31e6fe2ab
L E B Goodyear <goodyear.lucy@gmail.com>
2021-06-15 15:26:43 +0100

    add script to create R processing conda environment

---
+++ processing/conda_env_r_prcoessing.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=1:mem=10gb
+
+# Author: Luke Goodyear leg19@imperial.ac.uk
+# Script: env_manager.sh
+
+# Desc: 1) cleans conda environment 
+	2) creates new environment and installs packages
+	3) cleans conda environment
+# Arguments: None
+# Date: Jun 2021
+
+
+# load anaconda
+module load anaconda3/personal
+
+# remove any unused packages and caches
+conda clean -a -y
+
+# create new R conda environment
+conda create -n r_processing -c conda-forge r-base=4.1.0
+# activate new environment
+source activate r_processing
+
+# install required packages
+conda install -c bioconda bioconductor-dada2=1.20.0 -y
+conda install -c bioconda bioconductor-shortread=1.50.0 -y
+conda install -c bioconda bioconductor-biostrings=2.60.0 -y
+conda install -c bioconda bioconductor-phyloseq=1.36.0 -y
+conda install -c conda-forge r-dplyr=1.0.6 -y
+
+# remove any unused packages and caches after installations
+conda clean -a -y
+
+# view packages in environment as check
+conda list
+
+# deactivate environment
+conda deactivate
+
+
+# end of script
