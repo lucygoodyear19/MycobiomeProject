d761b783a35a1b0c4e599927cc6c2076d9f0ec90
L Goodyear <goodyear.lucy@gmail.com>
2021-06-17 13:57:01 +0100

    fix transpose row rename issue

--- processing/DADA2_data_filtering.R
+++ processing/DADA2_data_filtering.R
@@ -42,8 +42,8 @@ print(paste0("path_out: ", path_out))
 
 # load packages
 print("Loading required packages...")
-library(phyloseq)
-library(dplyr)
+library("phyloseq")
+library("dplyr")
 
 
 ################################# Load data #####################################
@@ -52,7 +52,7 @@ library(dplyr)
 print("Loading data...")
 
 # load taxa data
-tax <- read.table(paste0(path_in, "taxa_table.txt"), 
+tax <- read.table(paste0(path_in, "taxa_table.txt"),
                   stringsAsFactors = F,
                   header = T)
 # rename columns
@@ -105,7 +105,7 @@ print("Performing set up...")
 for (patt in 1:length(plate_patterns)) {
   plates$Sample_Name <- gsub(plate_patterns[patt], meta_patterns[patt], plates$Sample_Name)
 }
-#plates$Sample_Name <- gsub("_", "-", plates$Sample_Name)
+
 # country specific requirements: uncomment as required
 ## Vietnam
 #plates$Sample_Name <- gsub("-", "", plates$Sample_Name)
@@ -215,7 +215,7 @@ seqtab <- seqtab[,!names(seqtab) %in% mock_seqs]
 seqtab <- seqtab[!rownames(seqtab) %in% c("mock1", "mock2", "mock3", "mock4"),]
 
 
-##################### Controls
+##################### Negative controls
 
 
 print("Removing sequences found in negative controls...")
@@ -225,6 +225,10 @@ NCs <- c()
 for (samp in 1:nrow(plates)){
   if (grepl("NC", plates$Origin[samp])) {NCs <- c(NCs, plates$Sample_Name[samp])} 
 }
+
+
+################### Removing control reads
+
 
 # define function to remove posC and NC reads from samples by plate
 # returns a dataframe containing samples from the specifed plate, in the same form as df_abun 
@@ -260,26 +264,26 @@ filter_controls <- function(df_abun, df_plates, plate_no) {
 # transpose filtered abundance table
 seqtab_t <- as.data.frame(t(seqtab))
 # run the function on all plates
-seqtab_t_filtered <- data.frame(row.names=row.names(seqtab_t))
+seqtab_filtered <- data.frame(matrix(ncol=ncol(seqtab),nrow=0))
+colnames(seqtab_filtered) <- colnames(seqtab)
 for (plate in plate_nos) {
-  assign(paste0("plate", plate), filter_controls(seqtab_t, plates, plate))
+  assign(paste0("plate", plate), t(filter_controls(seqtab_t, plates, plate)))
   # combine the plates into one dataframe
-  seqtab_t_filtered <- cbind(seqtab_t_filtered, get(paste0("plate", plate)))
+  seqtab_filtered <- rbind(seqtab_filtered, get(paste0("plate", plate)))
 }
 
 # remove empty all samples (including posC and NC) from data frame
 # sum abundances by sample
-seqtab_t_filtered <- rbind(seqtab_t_filtered, apply(seqtab_t_filtered, 2, sum))
-# transpose dataframe
-seqtab_filtered <- as.data.frame(t(seqtab_t_filtered))
+seqtab_filtered <- cbind(seqtab_filtered, apply(seqtab_filtered, 1, sum))
+
 # count how many samples have no reads
-print(paste0("Number of sample with no reads after filtering: ", length(which(seqtab_filtered[,ncol(seqtab_filtered)] == 0))))
+print(paste0("Number of sample with no reads after filtering: ", length(which(seqtab_filtered[ncol(seqtab_filtered),] == 0))))
 
 print("Removing empty samples...")
 # save empty samples as a vector
 to_remove <- which(seqtab_filtered[,ncol(seqtab_filtered)] == 0)
 # remove empty samples from dataframe
-seqtab_filtered <- as.data.frame(seqtab_filtered[!rownames(seqtab_filtered) %in% names(to_remove),])
+seqtab_filtered <- seqtab_filtered[!rownames(seqtab_filtered) %in% names(to_remove),]
 # remove sum column
 seqtab_filtered <- seqtab_filtered[,-ncol(seqtab_filtered)]
 
@@ -299,7 +303,7 @@ seqtab_filtered <- seqtab_filtered[,which(seqtab_filtered[nrow(seqtab_filtered),
 seqtab_filtered <- seqtab_filtered[c(1:(nrow(seqtab_filtered)-1)),]
 
 # subset taxa table to include only the non-empty ASV assignments
-tax <- tax[which(rownames(tax) %in% names(seqtab_filtered)),]
+tax <- tax[which(rownames(tax) %in% names(as.data.frame(seqtab_filtered))),]
 
 
 ################################ create phyloseq object ################################
@@ -311,9 +315,6 @@ print("Saving to phyloseq object...")
 seqtab_mat <- as.matrix(seqtab_filtered)
 
 tax_mat <- as.matrix(tax) # required to create phyloseq object
-
-## Ecuador sample name change - comment out for all other runs
-#metadata$MiSeqCode <- gsub("^", "X", metadata$MiSeqCode)
 
 # create list of samples with no MiSeq code
 rm_id <- c()
